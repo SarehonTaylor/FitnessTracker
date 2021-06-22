a3bf389f4d50f0d7e3e4dfd8435d543023b71f2b
Sarehon Taylor <sarehontaylor@icloud.com>
2021-06-22 18:09:55 -0400

    adding values to the array

--- models/workout.js
+++ models/workout.js
@@ -2,3 +2,19 @@
 const mongoose = require('mongoose');
 //put everything for schema think mysql
 const Schema = require mongoose.Schema;
+
+const workoutSchema = new Schema ({
+    day: type: Date,
+    default: Date.now
+},
+
+exercises: [
+{
+    type: {
+        type: String,
+        trimL true,
+        required: 'Enter a type"'
+    }
+
+
+]

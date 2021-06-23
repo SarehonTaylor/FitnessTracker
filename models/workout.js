f53e78df130216c1a0f78a420f7f8712d69f983e
Sarehon Taylor <sarehontaylor@icloud.com>
2021-06-22 23:38:57 -0400

    tweaks to get heroku to show workout data working locally tho

--- models/workout.js
+++ models/workout.js
@@ -1,51 +1,48 @@
-// bring in mongoodb const schema
 const mongoose = require('mongoose');
-//put everything for schema think mysql
+
 const Schema = mongoose.Schema;
 
-const workoutSchema = new Schema ({
-    day:  {
-    type: Date,
-    default: Date.now
-},
-
-exercises: [
-{
-    type: {
-        type: String,
-        trim: true,
-        required: 'Enter a type'
+const workoutSchema = new Schema({
+    day: {
+        type: Date,
+        default: Date.now
     },
-    name: {
-        type: String,
-        trim: true,
-        required: 'Enter a name for your exercise'
-    },
-    distance: {
-        type: Number,
-        required: 'Enter a distance'
-    },
-    duration: {
-        type: Number,
-        required: 'Enter an amount'
-    },
-    weight: {
-        type: Number,
-        required: 'Enter a weight'
-    },
-    reps: {
-        type: Number,
-        required: 'Enter an amount'
-    },
-    sets: {
-        type: Number,
-        required: 'Enter an amount'
+    exercises: [
+        {
+            type: {
+                type: String,
+                trim: true,
+                required: 'Enter a type'
+            },
+            name: {
+                type: String,
+                trim: true,
+                required: 'Enter a name for your exercise'
+            },
+            distance: {
+                type: Number,
+                required: 'Enter a distance'
+            },
+            duration: {
+                type: Number,
+                required: 'Enter an amount'
+            },
+            weight: {
+                type: Number,
+                required: 'Enter a weight'
+            },
+            reps: {
+                type: Number,
+                required: 'Enter an amount'
+            },
+            sets: {
+                type: Number,
+                required: 'Enter an amount'
             }
         },
     ]
 });
-    
-const Workout = mongoose.model('Workout', workoutSchema);
 
+const Workout = mongoose.model('Workout', workoutSchema);
 
 module.exports = Workout;
